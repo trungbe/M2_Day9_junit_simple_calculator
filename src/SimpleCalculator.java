0d77769149b1644492af65286f42ff54f7f9a392
trungbe <trungnd0601@gmail.com>
2020-12-18 16:14:06 +0700

    test calculator

---
+++ src/SimpleCalculator.java
@@ -0,0 +1,14 @@
+public class SimpleCalculator {
+    public static int add(int first, int second) {
+        return first + second;
+//        throw new UnsupportedOperationException();
+    }
+
+    public static int sub(int first, int second) {
+        return first - second;
+//        throw new UnsupportedOperationException();
+    }
+
+}
+
+
